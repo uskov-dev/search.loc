852a637b70e30ebdc85c5ec28505e195429d652b
Ruslan Uskov <uskov.submail@gmail.com>
2022-03-09 14:03:04 +0300

    Верстка проекта

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="search">
+    <!-- <img src="./assets/images/loader-icon.svg" alt="" class="search__loader"> -->
+    <input type="text" value="" placeholder="Введите имя" class="search__input">
+  </div>
+  <div class="list">
+    <div class="list__content">
+      <div class="phrase list__phrase">
+        <div class="phrase__text">Петровна</div>
+        <button class="phrase__btn">Добавить</button>
+      </div>
+      <div class="phrase list__phrase">
+        <div class="phrase__text">Петров</div>
+        <button class="phrase__btn">Добавить</button>
+      </div>
+      <div class="phrase list__phrase">
+        <div class="phrase__text">Петро</div>
+        <button class="phrase__btn">Добавить</button>
+      </div>
+      <div class="phrase list__phrase">
+        <div class="phrase__text">Петр</div>
+        <button class="phrase__btn">Добавить</button>
+      </div>
+      <div class="phrase list__phrase">
+        <div class="phrase__text">Пет</div>
+        <button class="phrase__btn">Добавить</button>
+      </div>
+    </div>
+  </div>
+  <div class="list">
+    <h6 class="list__title">Список добавленных элементов:</h6>
+    <div class="list__content">
+      <div class="phrase list__phrase">
+        <div class="phrase__text">Петров</div>
+      </div>
+      <div class="phrase list__phrase">
+        <div class="phrase__text">Петровна</div>
+      </div>
+    </div>
+  </div>
+  <button class="stop-btn">
+    <span>Остановить автопоиск</span>
+  </button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '~/node_modules/normalize.css/normalize.css';
+@import '@/assets/sass/main.scss';
 </style>
